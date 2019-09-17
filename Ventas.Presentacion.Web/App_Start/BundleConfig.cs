30bbcd6c747cd62ceffe0561ba02d5293b4b230f
DanielCarbajal314 <daniel.carbajal@pucp.edu.pe>
2019-09-16 21:54:57 -0500

    Modal con vue-bootstrap

--- Ventas.Presentacion.Web/App_Start/BundleConfig.cs
+++ Ventas.Presentacion.Web/App_Start/BundleConfig.cs
@@ -11,10 +11,12 @@ namespace Ventas.Presentacion.Web
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/Galerias/Vue.js",
-                        "~/Scripts/Galerias/axios.js"));
+                        "~/Scripts/Galerias/axios.js",
+                        "~/Scripts/Galerias/bootstrap4-vue.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/Galerias/Bootstrap4.css",
+                      "~/Content/Galerias/bootstrap4-vue.css",                      
                       "~/Content/site.css"));
         }
     }
